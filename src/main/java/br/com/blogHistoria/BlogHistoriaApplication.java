d2853f76fbda1bd879cea8c211799e7618492996
ricardo <ricardoliveira50@hotmail.com>
2021-07-19 10:04:07 -0300

    Criação do projeto

---
+++ src/main/java/br/com/blogHistoria/BlogHistoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.blogHistoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHistoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogHistoriaApplication.class, args);
+	}
+
+}
