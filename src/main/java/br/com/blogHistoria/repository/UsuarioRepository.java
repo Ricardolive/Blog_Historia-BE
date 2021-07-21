ddeedd15a50c2b6e71a33f201aa41e6a3a3912e1
ricardo <ricardoliveira50@hotmail.com>
2021-07-21 11:26:22 -0300

    Criação do banco de dados

--- src/main/java/br/com/blogHistoria/repository/UsuarioRepository.java
+++ src/main/java/br/com/blogHistoria/repository/UsuarioRepository.java
@@ -10,6 +10,6 @@ import br.com.blogHistoria.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
-	public Optional<Usuario>findByEmail(String email);
+	public Optional<Usuario>findByEmail(String email); 
 
 }
